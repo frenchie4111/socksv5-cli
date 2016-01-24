930c7d5cc0222313faf05e3ad723e18b1eba0d87
Mike Lyons <mdl0394@gmail.com>
2016-01-23 22:27:26 -0500

    Added basic implementation of socksv5-cli

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-console.log( "Hello World" );
+var _ = require( 'underscore' );
+
+var startServer = require( './lib/server' ).startServer;
+
+var args = process.argv.slice( 2 );
+
+if( args.length !== 1 ) {
+	console.error( 'Ussage: socksv5 <path/to/config.json>' );
+	process.exit( 2 );
+}
+
+console.log( "Loading Config from: " + args[ 0 ] );
+var config = require( args[ 0 ] );
+
+if( _.isArray( config ) ) {
+	_
+		.each( config, function( server_config ) {
+			startServer( server_config );
+		} );
+} else {
+	startServer( server_config );
+}
+
