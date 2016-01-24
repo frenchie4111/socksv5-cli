37da6ca03082850636ddcd3894f33db46501282a
Mike Lyons <mdl0394@gmail.com>
2016-01-24 00:29:12 -0500

    Added localhost filter to generator

--- generator.js
+++ generator.js
@@ -8,12 +8,15 @@ var network_interfaces = os.networkInterfaces();
 var config = _
 	.chain( network_interfaces )
 	.filter( function( interface, name ) {
-		return( interface.length === 1 && !interface[ 0 ].internal && interface[ 0 ].family === 'IPv4' );
+		return( interface.length === 1 && 
+				!interface[ 0 ].internal && 
+				interface[ 0 ].family === 'IPv4' &&
+				interface[ 0 ].address !== '127.0.0.1' );
 	} )
 	.map( function( interface, name ) {
 		return {
 			host: interface[ 0 ].address,
-			port: '10' + interface[ 0 ].address.split( '.' )[ 3 ]
+			port: +( '10' + interface[ 0 ].address.split( '.' )[ 3 ] )
 		}
 	} )
 	.value();
