67383df4e83c7e35773fc2e66f934126e4fefa6f
Mike Lyons <mdl0394@gmail.com>
2016-01-24 00:26:56 -0500

    Added generator.js

---
+++ generator.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var os = require( 'os' ),
+	_ = require( 'underscore' );
+
+var network_interfaces = os.networkInterfaces();
+
+var config = _
+	.chain( network_interfaces )
+	.filter( function( interface, name ) {
+		return( interface.length === 1 && !interface[ 0 ].internal && interface[ 0 ].family === 'IPv4' );
+	} )
+	.map( function( interface, name ) {
+		return {
+			host: interface[ 0 ].address,
+			port: '10' + interface[ 0 ].address.split( '.' )[ 3 ]
+		}
+	} )
+	.value();
+
+console.log( JSON.stringify( config, null, 4 ) );
+
