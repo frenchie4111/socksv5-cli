d3a0f715fd6c70001587c89fc9d550bd4efa1a13
Michael Lyons <mdl0394@gmail.com>
2016-01-25 15:42:19 -0800

    Using fs load so that .conf files will work

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 #!/usr/bin/env node
 
-var _ = require( 'underscore' );
+var _ = require( 'underscore' ),
+	fs = require( 'fs' );
 
 var startServer = require( './lib/server' ).startServer;
 
 var args = process.argv.slice( 2 );
 
-if( args.length !== 1 ) {
-	console.error( 'Ussage: socksv5 <path/to/config.json>' );
+var exitAndPrint = function( error ) {
+	console.error( error );
 	process.exit( 2 );
 }
 
+if( args.length !== 1 ) {
+	exitAndPrint( 'Ussage: socksv5 <path/to/config.json>' );
+}
+
 console.log( "Loading Config from: " + args[ 0 ] );
-var config = require( args[ 0 ] );
+
+var config; 
+
+try {
+	config = fs.readFileSync( args[ 0 ] );
+} catch( e ) {
+	console.error( e );
+	exitAndPrint( 'Failed to open: ' + args[ 0 ] );
+}
+
+try {
+	config = JSON.parse( config );
+} catch( e ) {
+	exitAndPrint( 'Failed to parse: ' + args[ 0 ] );
+}
 
 if( _.isArray( config ) ) {
 	_
