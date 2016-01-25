ecfc0c46671fa87bacf666a792c451794aa53f10
Mike Lyons <mdl0394@gmail.com>
2016-01-25 15:10:39 -0800

    Added root check to install_debian.sh

--- install_debian.sh
+++ install_debian.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Install sudo
-apt-get install -y sudo
+# Check if root
+if [[ $EUID -ne 0 ]]; then
+   echo "Please run this script as root" 
+   exit 1
+fi
 
 # Install Nodev4
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_4.x | bash -
+apt-get update
 apt-get install -y nodejs build-essential
 
 # Install socksv5-cli
 npm install -g socksv5-cli
-
